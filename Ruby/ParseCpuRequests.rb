796475543a558905be9166244158ca993de4d9c6
steelead <steelead@pdx.edu>
2016-03-04 05:05:12 -0800

    closed page policy function
    - Added array for previous commands and logic for using it (updating values every DRAM clock tick and updating when new DRAM command executed)
    - David and I figured out the missing 3 bits we had in the address (chunk select) so we added that in
    - Modified the getCommandSequence function a bit to pass parameters
    - Added exit program if the instruction is invalid
    - removed count variable
    - Modified checkInstruction function since now that all 32 bits of address are being used, the row bank and column will always be within the range (need to add a check to make sure address is 32 bits though)
    - Added function to check if it is ok to execute DRAM command

--- Ruby/ParseCpuRequests.rb
+++ Ruby/ParseCpuRequests.rb
@@ -10,18 +10,29 @@ class ParseCpuRequests
   $goFetch = 1
   $traceFile = Array.new
 
+  $prevCommands = Array.new
+  for i in 0..7
+    $prevCommands.push(Hash['ACT' => Float::INFINITY,
+                            'PRE' => Float::INFINITY,
+                            'RD' => Float::INFINITY,
+                            'RDAP' => Float::INFINITY,
+                            'WR' => Float::INFINITY,
+                            'WRAP' => Float::INFINITY,
+                            'REF' => Float::INFINITY])
+  end
+
   #loads all each line from the external file into an array called $fileRequest
   def getOneRequestFromFile
     in_line = $file.gets
     if(in_line != nil)
-
       address = in_line.sub(/0x/, '').split(/\W+/)[0].to_i(16).to_s(2).reverse
 
       #parse instruction into a hash: row,bank,col,instruction,cpuTime
       $fileRequest << {
-                  "row" => address.split(//)[14..31].join('').reverse.to_i(2),
-                  "bank" => address.split(//)[11..13].join('').reverse.to_i(2),
-                  "col" => address.split(//)[0..10].join('').reverse.to_i(2),
+                  "row" => address.split(//)[17..31].join('').reverse.to_i(2),
+                  "bank" => address.split(//)[14..16].join('').reverse.to_i(2),
+                  "col" => address.split(//)[3..13].join('').reverse.to_i(2),
+                  "chunk" => address.split(//)[0..2].join('').reverse.to_i(2),
                   "inst" => in_line.split(/\W+/)[1],
                   "cpuTime" => in_line.split(/\W+/)[2].to_i
                   }
@@ -36,7 +47,7 @@ class ParseCpuRequests
 
   def simulateDRAMMemController
     begin
-          #cpu clock
+      #cpu clock
       $CpuClock = $CpuClock + 1
 
       if($goFetch == 1 && !$file.eof?())
@@ -44,70 +55,82 @@ class ParseCpuRequests
         $goFetch = 0
       end
 
-      if(!$fileRequest.empty?())
+      if not $fileRequest.empty?
       #only loads the Memory Controller buffer when it's less or equal to than 16 and proper CPU time.
         if(($CPUBuffer.size() <= 16) && ($fileRequest.first["cpuTime"] == $CpuClock) )#&& !$file.eof?())
-
           temp = $fileRequest.shift
 #          $CPUBuffer << $fileRequest.shift
           if (checkInstruction(temp))
             $CPUBuffer << temp
+            $CPUBuffer.last['DRAMCommands'] = getCommandSequence($CPUBuffer.last['inst'])
+            puts $CPUBuffer.last
           else
              puts "Instruction Not Queued"
+             exit
           end
           $goFetch = 1
         end
       end
 
       #the buffers finishes one Memory request at every 200 CPU cycles (4x50 = 200)
-      if((!$CPUBuffer.empty?()))
-        $count = $count + 1
+      if($CpuClock%4 == 0)
+        $DRAMClock = $DRAMClock + 1
 
-        if($count%4 == 0)
-          $DRAMClock = $DRAMClock + 1
+        #each command in prevCommands array is now 1 DRAM clock older
+        for bankCommands in $prevCommands
+          for command in bankCommands.keys
+            bankCommands[command] += 1
+          end
         end
 
-        #first command will take 50 DRAM cycles
-        if($DRAMClock == 50)
-          #getCommandSequence
-          $CPUBuffer.shift
-          $DRAMClock = 0
+        #only check for dram commands if there are items in the buffer
+        if not $CPUBuffer.empty?
+          #first command will take 50 DRAM cycles
+          if tryDRAMCommand($CPUBuffer[0]['DRAMCommands'].first, $CPUBuffer[0]['bank'], $CPUBuffer[0]['row'], $CPUBuffer[0]['col']) == true
+            #output dram command
+            puts "DRAM Command issued: %s   Bank: %d   Row: %d   Column: %d" % [$CPUBuffer[0]['DRAMCommands'].first, $CPUBuffer[0]['bank'], $CPUBuffer[0]['row'], $CPUBuffer[0]['col']]
+
+            #update prevCommands array
+            $prevCommands[$CPUBuffer[0]['bank']][$CPUBuffer[0]['DRAMCommands'].first] = 0
+
+            #remove dram command from dram command array
+            $CPUBuffer[0]['DRAMCommands'].shift
+
+            if $CPUBuffer[0]['DRAMCommands'].empty?
+              #remove item from CPUBuffer (all dram commands for this cpu request have been completed)
+              $CPUBuffer.shift
+            end
+          end
         end
       end
-      #puts "%4d %4d %3d %2d %s" % [$CpuClock, $count, $DRAMClock, $CPUBuffer.size(), $CPUBuffer.last]
+
+      puts "%4d %3d %2d %s" % [$CpuClock, $DRAMClock, $CPUBuffer.size(), $CPUBuffer.last]
     end while (!$CPUBuffer.empty?() or !$file.eof?())
   end
 
-  def getCommandSequence
+  def getCommandSequence(requestType)
+    commands = Array.new
 
     if ($CPUBuffer[0]["inst"] == "READ")
-      $CPUBuffer[0].store("command1", "ACT")
-      $CPUBuffer[0].store("command2", "RDAP")
-      puts $CPUBuffer[0]
+      commands.push("ACT")
+      commands.push("RDAP")
+
     elsif ($CPUBuffer[0]["inst"] == "WRITE")
-      $CPUBuffer[0].store("command1", "ACT")
-      $CPUBuffer[0].store("command2", "WRAP")
-      puts $CPUBuffer[0]
+      commands.push("ACT")
+      commands.push("WRAP")
+
     else
-      $CPUBuffer[0].store("command1", "ACT")
-      $CPUBuffer[0].store("command2", "RDAP")
-      puts $CPUBuffer[0]
+      commands.push("ACT")
+      commands.push("RDAP")
     end
+
+    return commands
 
   end
 
   def checkInstruction(inInstruction)
     #print "#{inInstruction} \n"
-    if((inInstruction["row"] < 0) or (inInstruction["row"] > 2**15))
-      puts "Row is not within the DRAM limit of 0-%d. #{inInstruction["row"]}" % [2**15]
-      return false
-    elsif ((inInstruction["bank"] < 0) or (inInstruction["bank"] > 2**3))
-      puts "Bank is not wihtin the DRAM limit of 0-%d" % [2**3]
-      return false
-    elsif ((inInstruction["col"] < 0) or (inInstruction["col"] > 2**11))
-      puts "Column is not within the DRAM limit of 0- %d" % [2**11]
-      return false
-    elsif ((inInstruction["inst"].downcase != "read") &
+    if ((inInstruction["inst"].downcase != "read") &
             (inInstruction["inst"].downcase != "write") &
             (inInstruction["inst"].downcase != "ifetch"))
       puts "#{inInstruction["inst"]} is NOT a valid Instruction"
@@ -117,6 +140,48 @@ class ParseCpuRequests
       return false
     else
       return true
+    end
+  end
+
+  def tryDRAMCommand(dramCommand, bank, row, column)
+    tRC = 50
+    tRAS = 36
+    tRRD = 6
+    tRP = 14
+    tRFC = 172
+    tCWL = 10
+    tCAS = 14
+    tRCD = 14
+    tWR = 16
+    tRTP = 8
+    tCCD = 4
+    tBURST = 4
+    tWTR = 8
+
+    if dramCommand == "ACT"
+      if $prevCommands[bank]['ACT'] >= tRC
+        return true
+      else
+        return false
+      end
+
+    elsif dramCommand == "RDAP"
+      if $prevCommands[bank]['ACT'] >= tRCD
+        return true
+      else
+        return false
+      end
+
+    elsif dramCommand == "WRAP"
+      if $prevCommands[bank]['ACT'] >= tRCD
+        return true
+      else
+        return false
+      end
+
+    else
+      puts "command: %s not yet supported" % dramCommand
+
     end
   end
 end
